49c48ccf6ba30f28142013397ce6e2d2c5dee617
Batuhan Demir <41114063+batudmr@users.noreply.github.com>
2022-02-18 02:10:29 +0300

    Add auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import { currentUserRouter } from './routes/current-user';
+import { singInRouter } from './routes/signin';
+import { singOutRotuer } from './routes/signout';
+import { signUpRouter } from './routes/singup';
 import { json } from 'body-parser';
 
 const app = express();
 app.use(json());
+
+app.use(currentUserRouter);
+app.use(singInRouter);
+app.use(singOutRotuer);
+app.use(signUpRouter);
 
 app.listen(3000, () => {
   console.log('Listening on 3000');
---
+++ auth/src/routes/signout.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/api/users/singout', (req, res) => {});
+
+export { router as singOutRotuer };
