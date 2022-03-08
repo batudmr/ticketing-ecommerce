8fb5ed97288ea502e897700941db0c92ef81c5d0
Batuhan Demir <41114063+batudmr@users.noreply.github.com>
2022-03-08 20:44:58 +0300

    Add NATS streaming server to handle events

---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,17 @@
+import nats from 'node-nats-streaming';
+
+console.clear();
+
+const stan = nats.connect('ticketing', '123', { url: 'http://localhost:4222' });
+
+stan.on('connect', () => {
+  console.log('Publisher connected to Nats');
+  const data = JSON.stringify({
+    id: '123',
+    title: 'concert',
+    price: 20,
+  });
+  stan.publish('ticket:created', data, () => {
+    console.log('Event published');
+  });
+});
